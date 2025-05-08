7c3785f721ee073e969e926ebc3bb28ecfd0ef92
matanklein <157304183+matanklein@users.noreply.github.com>
2025-05-08 17:51:51 +0000

    changing the interface

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-ifconfig wlxe84e06afb969 down
-iwconfig wlxe84e06afb969 mode Monitor
-ifconfig wlxe84e06afb969 up
+# This script is used to set up the wireless interface for monitor mode
+# Assuming the wireless interface is wlxe84e06aed7ca
+
+ifconfig wlxe84e06aed7ca down
+iwconfig wlxe84e06aed7ca mode Monitor
+ifconfig wlxe84e06aed7ca up
