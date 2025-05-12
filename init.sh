d2ee4a1bbea3a0ab69bb49c49a8af963ca5e266d
matanklein <matan.klein1@msmail.ariel.ac.il>
2025-05-12 13:43:02 -0400

    Adding sudo

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 # This script is used to set up the wireless interface for monitor mode
 # Assuming the wireless interface is wlxe84e06aed7ca
 
-ifconfig wlxe84e06aed7ca down
-iwconfig wlxe84e06aed7ca mode Monitor
-ifconfig wlxe84e06aed7ca up
+sudo ifconfig wlxe84e06aed7ca down
+sudo iwconfig wlxe84e06aed7ca mode Monitor
+sudo ifconfig wlxe84e06aed7ca up
